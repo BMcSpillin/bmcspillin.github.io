4b6c1a0c54f6dda2f20981d117ade3d51c9d0627
Bridget McMillan <bridgetmc09@gmail.com>
2016-05-11 14:33:28 -0400

    added TriBeca Upholstery and Draperies

--- assets/fewd_final/js/main.js
+++
@@ -1,8 +0,0 @@
-$("a").hover(function(){
-    $(this).css("opacity", ".6");
-    }, function(){
-    $(this).css("opacity", "1");
-});
-
-//EM: I encourage you to integrate a couple of additional javascript features to increase the 
-//complexity here. I really like the idea of the moving bicycles and that would not be difficult to do
